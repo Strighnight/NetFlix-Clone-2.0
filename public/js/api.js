fb9bb20c105db7e426dbd726c3ac190dd645ad34
Luan Mercaldi <134025471+Strighnight@users.noreply.github.com>
2024-07-31 14:40:19 -0300

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,7 +1,4 @@
-let api_key = "d19a1946970f98fae002af7545322879"
-
-let img_url = "https://image.tmdb.org/t/p/w500"
-
-let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
-
-let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+const API_KEY = "d19a1946970f98fae002af7545322879";
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const GENRES_LIST_URL = `https://api.themoviedb.org/3/genre/movie/list?`;
+const MOVIE_GENRES_URL = `https://api.themoviedb.org/3/discover/movie?`;
