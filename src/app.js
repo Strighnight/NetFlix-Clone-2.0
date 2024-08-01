abf6871dc4c9f8af8733fcc66d75450ff88d5bc1
Luan Mercaldi <134025471+Strighnight@users.noreply.github.com>
2024-08-01 20:27:56 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 // app.js
+
+// Importa os módulos necessários
 const express = require("express");
 const path = require("path");
 
+// Função para criar a aplicação Express
 const createApp = () => {
   const app = express();
-  const publicDirectoryPath = path.join(__dirname, "public");
 
+  // Define o diretório público para servir arquivos estáticos
+  const publicDirectoryPath = path.join(__dirname, "public");
   app.use(express.static(publicDirectoryPath));
 
+  // Rota principal que serve o arquivo index.html
   app.get("/", (req, res) => {
     const indexPath = path.join(publicDirectoryPath, "index.html");
     res.sendFile(indexPath);
   });
 
+  // Retorna a instância da aplicação Express
   return app;
 };
 
+// Exporta a função createApp para uso em outros módulos
 module.exports = createApp;
